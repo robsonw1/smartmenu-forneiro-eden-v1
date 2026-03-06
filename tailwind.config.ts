a478d3a4ad3cde1fdcd5929a904a848383147976
robsonw1 <robsonwilliam1008@gmail.com>
2026-03-05 23:19:06 -0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,7 @@ export default {
     extend: {
       fontFamily: {
         display: ['"Quicksand"', 'sans-serif'],
+        heading: ['"Quicksand"', 'sans-serif'],
         sans: ['Inter', 'sans-serif'],
       },
       colors: {
